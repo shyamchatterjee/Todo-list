70499edf3fc1f7cd86546a53c9005445d21ccbed
shyamdyuti <chatterjeeshyamdyuti@gmail.com>
2025-03-06 12:56:22 +0530

    form design complite

--- src/Component/form.jsx
+++ src/Component/form.jsx
@@ -1,8 +1,8 @@
 let Form = ()=>{
   return  <form className="form">
-       <input type="text" name="" id="" />
-       <input type="date" name="" id="" />
-       <input type="submit" value="Add" />
+       <input type="text" name="" id="input-text" placeholder="Enter todo name" />
+       <input type="date" name="" id="input-date" />
+       <input className="submit-button" type="submit" value="Add" />
   </form>
 }
 export  default Form
