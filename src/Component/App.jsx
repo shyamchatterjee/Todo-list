8347a4397a41ca5d332337d7e0addd79755efeb7
shyamdyuti <chatterjeeshyamdyuti@gmail.com>
2025-03-06 12:25:11 +0530

    error solving

--- src/Component/App.jsx
+++ src/Component/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from 'react'
 
 import '../App.css'
+import Form from "./form";
 
 function App() {
   return <div className='contener'>
-          
+             <Form/>
   </div>
 }
 
--- src/Component/form.jsx
+++ src/Component/form.jsx
@@ -1,4 +1,8 @@
 let Form = ()=>{
-
+  return  <form className="form">
+       <input type="text" name="" id="" />
+       <input type="date" name="" id="" />
+       <input type="submit" value="Add" />
+  </form>
 }
 export  default Form
