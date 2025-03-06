77643665a03c031663ae62b4375197f082faab83
shyamdyuti <chatterjeeshyamdyuti@gmail.com>
2025-03-06 13:08:05 +0530

    css design

--- src/Component/App.jsx
+++ src/Component/App.jsx
@@ -2,10 +2,12 @@ import { useState } from 'react'
 
 import '../App.css'
 import Form from "./form";
+import Todocontener from './todocontener';
 
 function App() {
   return <div className='contener'>
              <Form/>
+             <Todocontener/>
   </div>
 }
 
--- src/Component/todocontener.jsx
+++ src/Component/todocontener.jsx
@@ -1,4 +1,8 @@
 let Todocontener = ()=>{
+  return <div className="todo-contener">
+            <div className="todo">
 
+            </div>
+  </div>
 }
 export default Todocontener
